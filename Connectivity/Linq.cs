72806a69ed07841886ce9a99b4ff67040b7e0ce6
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-06-16 11:01:20 +0700

    Add Method Syntax

--- Connectivity/Linq.cs
+++ Connectivity/Linq.cs
@@ -1,16 +1,40 @@
-﻿namespace Connectivity
+﻿using System.Data.SqlClient;
+
+namespace Connectivity
 {
     class Linq
     {
         public void GetTugas1()
         {
+            SqlConnection conn = MyConnection.Get();
             var employee = new Employee();
             var department = new Department();
             var location = new Location();
             var country = new Country();
             var region = new Region();
 
-            var employess = (from e in employee.GetAllEmployee()
+            // Method Syntax
+            var employees = employee.GetAllEmployee()
+                            .Join(department.GetAllDepartment(), e => e.DepartmentId, d => d.Id, (e, d) => new { e, d })
+                            .Join(location.GetAllLocation(), ed => ed.d.LocationId, l => l.Id, (ed, l) => new { ed.e, ed.d, l })
+                            .Join(country.GetAllCountry(), edl => edl.l.CountryId, c => c.Id, (edl, c) => new { edl.e, edl.d, edl.l, c })
+                            .Join(region.GetAllRegion(), edlc => edlc.c.RegionId, r => r.Id, (edlc, r) => new { edlc.e, edlc.d, edlc.l, edlc.c, r })
+                            .Select(emp => new
+                            {
+                                ID = emp.e.Id,
+                                fullName = string.Concat(emp.e.FirstName, " ", emp.e.LastName),
+                                Email = emp.e.Email,
+                                Phone = emp.e.PhoneNumber,
+                                Salary = emp.e.Salary,
+                                Department = emp.d.Name,
+                                Address = emp.l.StreetAddress,
+                                Country = emp.c.Name,
+                                Region = emp.r.Name
+                            })
+                            .Take(5);
+
+            // Query Syntax
+            /*var employess = (from e in employee.GetAllEmployee()
                              join d in department.GetAllDepartment() on e.DepartmentId equals d.Id
                              join l in location.GetAllLocation() on d.LocationId equals l.Id
                              join c in country.GetAllCountry() on l.CountryId equals c.Id
@@ -26,9 +50,9 @@
                                  Address = l.StreetAddress,
                                  Country = c.Name,
                                  Region = r.Name
-                             }).Take(5);
+                             }).Take(5);*/
             Console.Clear();
-            foreach (var emp in employess)
+            foreach (var emp in employees)
             {
                 Console.WriteLine();
                 Console.WriteLine($"ID\t\t : {emp.ID}");
@@ -52,7 +76,23 @@
             var employee = new Employee();
             var department = new Department();
 
-            var employees = from d in department.GetAllDepartment()
+            // Method Syntax
+            var employees = department.GetAllDepartment()
+                            .Join(employee.GetAllEmployee(), d => d.Id, e => e.DepartmentId, (d, e) => new { Department = d, Employee = e })
+                            .GroupBy(x => x.Department.Name)
+                            .Where(g => g.Count() > 3)
+                            .Select(g => new
+                            {
+                                DeptName = g.Key,
+                                TotalEmployee = g.Count(),
+                                MinSalary = g.Min(e => e.Employee.Salary),
+                                MaxSalary = g.Max(e => e.Employee.Salary),
+                                Average = g.Average(e => e.Employee.Salary)
+                            });
+
+
+            // Query Syntax
+            /*var employees = from d in department.GetAllDepartment()
                             join e in employee.GetAllEmployee() on d.Id equals e.DepartmentId
                             group e by d.Name into eGroup
                             where eGroup.Count() > 3
@@ -63,7 +103,7 @@
                                 MinSalary = eGroup.Min(e => e.Salary),
                                 MaxSalary = eGroup.Max(e => e.Salary),
                                 Average = eGroup.Min(e => e.Salary),
-                            };
+                            };*/
             Console.Clear();
             foreach (var emp in employees)
             {
@@ -83,8 +123,8 @@
             Console.Clear();
             Console.WriteLine("     Menu LINQ     ");
             Console.WriteLine("--------------");
-            Console.WriteLine("1. Tugas Nomor 1");
-            Console.WriteLine("2. Tugas Nomor 2");
+            Console.WriteLine("1. Task 1");
+            Console.WriteLine("2. Task 2");
             Console.WriteLine("3. Exit");
 
             try
