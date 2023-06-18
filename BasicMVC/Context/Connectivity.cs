3168295e96c30571c652fec23a781e5b6d34c3a3
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-06-18 19:49:28 +0700

    Update Connectivity.cs

--- BasicMVC/Context/Connectivity.cs
+++ BasicMVC/Context/Connectivity.cs
@@ -4,7 +4,7 @@ namespace BasicMVC.Context
 {
     public class Connectivity
     {
-        private static string connectionString = "Data Source=DESKTOP-8I1UG4L;Database = db_hr1;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+        private static string connectionString = "Data Source=DESKTOP-8I1UG4L;Database = db_hr;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
         public static SqlConnection Connection()
         {
             return new SqlConnection(connectionString);
